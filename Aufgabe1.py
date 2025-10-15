7e20c2041953b4c813b11ec6f7c868e2170d7ae3
xFaNa <j.n.neugebauer@gmail.com>
2025-10-15 17:49:54 +0200

    Initial commit: Dateien und Projektstruktur hinzugefügt

---
+++ Aufgabe1.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+# Beispiel-Daten
+monate = ["Jan", "Feb", "Mae", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"]
+temperatur_deutschland = [1.5, 6.6, 7.5, 10.1, 15.1, 16.8, 18.8, 19.9, 15.4, 11.0, 5.1, 3.0]
+temperatur_australien = [27, 26, 23, 19, 15, 11, 11, 12, 15, 19, 22, 25]
+
+
+# Diagramm erstellen
+plt.plot(monate, temperatur_deutschland, label="Deutschland", color="blue", marker="")
+plt.plot(monate, temperatur_australien, label="Australien", color="red", marker="")
+
+plt.xlabel("Monate")
+plt.ylabel("Temperatur in Grad °C")
+plt.title("durchschnitlliche Monatstemperaturen")
+plt.legend()
+plt.grid(True)
+
+# Diagramm anzeigen
+plt.show()
+
+# Diagramm speichern (nach dem Erstellen, aber vor plt.show() ist auch möglich
---
+++ README.md
@@ -0,0 +1,7 @@
+# Praktikumsaufgaben für das Fach Datenanalyse & ML
+
+Dieses Repository enthält die Lösungen für die verschiedenen Praktikumsaufgaben.
+
+## Vorraussetzungen
+- Python 3.12 oder höher
+- Bibliotheken 'pandas', 'matplotlib'
